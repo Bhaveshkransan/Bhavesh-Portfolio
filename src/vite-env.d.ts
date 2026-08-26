72c01e8b7eff0ddf87d22a709e67d371710919e6
Bhavesh Gangurde <bhaveshg1357@gmail.com>
2026-08-26 21:13:09 +0530

    fix: replace deprecated gsap-trial with official standard gsap plugins to eliminate membership redirect

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="vite/client" />
+declare module 'gsap/SplitText';
+declare module 'gsap/ScrollSmoother';
 declare module 'gsap-trial/SplitText';
 declare module 'gsap-trial/ScrollSmoother';
